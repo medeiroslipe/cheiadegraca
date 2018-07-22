ca2a302f76e7dbfa0af8067bdb2720efd7723169
Medeiros Felipe <medeiroslip@gmail.com>
2018-07-22 02:37:40 -0300

    alteração no arquivo javascript script.js pegando em forma de array as div's onde vão ficar as imagens

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-$("div").load + "teste";
+var $cardFotos = $("div.cardFotos");
+var caminho="../cheiadegraca/img/biquini";
+var links=[];
+ console.log(links)
+
+for( var i=0; i<$cardFotos.length; i++){
+    links += caminho + $cardFotos[i]++;
+    console.log(links)
+   
+}
